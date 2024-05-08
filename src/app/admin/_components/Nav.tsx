a22b39258fd8a614e1bd58b51ae91e1a6c099598
Valkon <96625039+Nawija@users.noreply.github.com>
2024-05-08 12:47:18 +0200

    gfd

--- src/app/admin/_components/Nav.tsx
+++ src/app/admin/_components/Nav.tsx
@@ -3,9 +3,8 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { ComponentProps, ReactNode } from "react";
 
-export function Nav({ children }: { children: ReactNode }) {
+export function Nav({ children }: { children: React.ReactNode }) {
     return (
         <nav className="bg-primary text-primary-foreground flex lg:flex-col justify-center lg:justify-start lg:pt-4 px-4 pr-0 lg:min-h-screen">
             {children}
@@ -13,7 +12,9 @@ export function Nav({ children }: { children: ReactNode }) {
     );
 }
 
-export function NavLink(props: Omit<ComponentProps<typeof Link>, "className">) {
+export function NavLink(
+    props: Omit<React.ComponentProps<typeof Link>, "className">
+) {
     const pathname = usePathname();
     return (
         <Link
